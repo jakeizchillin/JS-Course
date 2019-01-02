8bce92b7b1bc019d59c30079e2b95a5a5e8c8e9c
Jacob Peterman <jpeterman@yp.com>
2019-01-02 15:44:15 -0800

    Section 2 Lecture 10

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,6 +1,6 @@
 /***************
 * Variable mutation and type coercion
-*/
+
 
 var firstName = 'John';
 var age = 28;
@@ -19,5 +19,34 @@ job = 'driver';
 
 alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
+// grab name from prompt
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
+
+/*******************
+ * Basic opperators
+*/
+var now, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math opperators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical opperators
+var johnOlder = ageJohn >  ageMark;
+console.log(johnOlder);
+
+// typeof opperator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof x);
+var x;
