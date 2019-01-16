b0bf14a1ffdc858d4afe97c4c9a344fb8f66fa1b
jacob peterman <jake.peterman@gmail.com>
2019-01-15 21:56:50 -0800

    Functions

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -167,7 +167,7 @@ if (markBMI > johnBMI) {
 
 /***************
 * Boolean logic
-*/
+
 
 var firstName = 'John';
 var age = 16;
@@ -179,3 +179,61 @@ if (age < 13) {
 } else {
     console.log(firstName + ' is a man.');
 }
+
+*/
+
+/*****************
+ * Functions
+
+
+ function calculateAge(birthYear) {
+     return 2018 - birthYear;
+ }
+
+ var ageJohn = calculateAge(1990);
+ var ageMike = calculateAge(1948);
+ var ageJane = calculateAge(1969);
+ console.log(ageJohn, ageMike, ageJane);
+
+ function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    
+    if (retirement > 0) {
+        console.log(firstName + ' retires in ' + retirement + ' years.' );
+    } else {
+        console.log(firstName + ' is already retired.');
+    }
+ }
+
+ yearsUntilRetirement(1990, 'John');
+ yearsUntilRetirement(1948, 'Mike');
+ yearsUntilRetirement(1969, 'Jane');
+
+  */
+
+/****************************
+ *  Function Statements and Expressions
+ * 
+ */
+
+ // function declaration
+ // function whatDoYouDo(job, firstName) {}
+
+// function expression
+  var whatDoYouDo = function(job, firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code.';
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon.';
+        case 'designer':
+            return firstName + ' designs beautiful websites.';
+        default:
+            return firstName + ' does something else.';
+    }
+  }
+
+  console.log(whatDoYouDo('teacher', 'John'));
+  console.log(whatDoYouDo('designer', 'Jane'));
+  console.log(whatDoYouDo('retired', 'Mark'));
