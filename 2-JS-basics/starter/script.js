d24618eb09f4aeed7a9b2a9b3f2c5817f9ac2120
Jacob Peterman <jpeterman@yp.com>
2019-01-23 16:34:52 -0800

    29 loops and iteration

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -476,7 +476,6 @@ console.log(john);
 */
 /******************
  * Coding Challenge 4
- */
 
 var john = {
     fullName: 'John',
@@ -509,6 +508,65 @@ if (john.calcBMI() === mark.calcBMI()) {
 } else {
     console.log('Mark\'s BMI is greater than John\'s at ' + mark.bmi + '.');
 }
+
+*/
+
+/****************
+ * Loops and iteration
+ */
+
+// for loop
+/*
+for (var i = 0; i < 10; i+= 2) {
+    console.log(i);
+} 
+
+// i = 0 counter begin, condition evaluated, counter updated
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
+
+// while loop
+var i = 0;
+while (i > john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+// continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+/*
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
