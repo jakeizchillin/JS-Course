fe7e92acd486c896b894f308840bc827e69f6921
Jacob Peterman <jpeterman@yp.com>
2019-02-05 10:21:11 -0800

    Scoping

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,30 +1,42 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+calculateAge(1965);
 
 
+function calculateAge(year) { // stored in variable object before anything 
+    console.log(2016 - year); // only works for function declarations
+}
+
+// retirement(1990); // does not work, function expression
 
 
+var retirement  = function(year) {  // function expression
+    console.log(65 - (2016 - year));
+}
 
+// variables
 
+// console.log(age); this is undefined - doc is scanned first and variables are set to undefined
+var age = 23;
+console.log(age);
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo(); // 65 printed
+console.log(age); // prints 23 b/c global execution context 
 
-
-
-
-
-
-
-
-
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
 
 
-// First scoping example
-
+// First scoping example - each function creates a scope
 /*
-var a = 'Hello!';
+var a = 'Hello!'; // Global scope
 first();
 
 function first() {
@@ -36,13 +48,12 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
 
+*/
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
 var a = 'Hello!';
 first();
 
@@ -60,8 +71,6 @@ function third() {
     var d = 'John';
     console.log(a + b + c + d);
 }
-*/
-
 
 
 ///////////////////////////////////////
