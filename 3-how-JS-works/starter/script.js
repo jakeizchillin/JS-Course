5a823024e5e4556f7b51b3f44a34b2faa7aab775
jacob peterman <jake.peterman@gmail.com>
2019-02-07 21:41:06 -0800

    last of section 3

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -53,7 +53,7 @@ function first() {
 
 
 // Example to show the differece between execution stack and scope chain
-
+/*
 var a = 'Hello!';
 first();
 
@@ -76,9 +76,42 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this); // prints window object
 
+calculateAge(1985);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);
+}
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        
+    }
+}
+/
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
+*/
 
 
 
