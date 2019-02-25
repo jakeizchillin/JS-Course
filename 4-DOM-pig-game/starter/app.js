a95bcfabc5920756c774b75a91a0ec1214ed3890
Jacob Peterman <jpeterman@yp.com>
2019-02-25 15:28:48 -0800

    Pig Game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -16,14 +16,13 @@ roundScore = 0;
 activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 var x = document.querySelector('#score-0').textContent;
 console.log(x);
 
-document.querySelector('.dice').style.display = 'none'; // style is css . is property
-
-
+document.querySelector('.dice').style.display = 'none';
 
