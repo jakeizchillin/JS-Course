86215e5c824de1050c904fd28de3b1a5d330d1b8
Jacob Peterman <jpeterman@yp.com>
2019-05-13 21:15:12 -0700

    Lecture 9

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,6 @@
-
+/**
+ * Variables and data types
+ *
 var firstName = 'John';
 console.log(firstName);
 
@@ -14,4 +16,31 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
+// Variable naming rules
 var 3years = 3;
+*/
+
+/**
+ * Variable mutation and type coercion
+ */
+
+ var firstName = 'John';
+ var age = 28;
+
+ // Type coercsion
+ console.log(firstName + ' ' + age);
+
+ var job, isMarried;
+ job = 'teacher';
+ isMarried = false;
+
+ console.log(firstName + ' is a ' + age + ' year old ' + job + '.  Is he married? ' + isMarried);
+
+ // Variable mutation
+ age = 'tweny eight';
+ job = 'driver';
+
+ alert(firstName + ' is a ' + age + ' year old ' + job + '.  Is he married? ' + isMarried);
+
+ var lastName = prompt('What is his last name?');
+ console.log(firstName + ' ' + lastName);
