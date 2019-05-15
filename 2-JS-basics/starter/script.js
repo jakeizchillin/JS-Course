00ab5a4f2b328b3912de1a0ea9bbba48c602a7c8
Jacob Peterman <jpeterman@yp.com>
2019-05-15 16:02:38 -0700

    lesson 11

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -49,7 +49,7 @@ var 3years = 3;
 
  /************************************
   * Basic operators
-  */
+
 
 var year, yearJohn, yearMark;
 now = 2018;
@@ -74,3 +74,37 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var x;
 console.log(typeof x);
+
+*/
+
+/**
+ * Operator precedence
+
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge =  now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+// Grouping
+var ageJohn  = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 //
+console.log(x, y);
+
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x-- ;
+console.log(x);
+
+*/
