e641c6c9f84756dd8cf26c6e2492b4ed615784ca
jacob peterman <jake.peterman@gmail.com>
2019-05-27 15:17:17 -0700

    Overdue

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -108,3 +108,23 @@ x-- ;
 console.log(x);
 
 */
+
+/**
+ * 14 If/else statements
+ */
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+  console.log(firstName + ' is married!');
+} else {
+  console.log(firstName + ' will hopefully marry soon.')
+}
+
+var isMarried = true;
+if (isMarried) {
+  console.log(firstName + ' is married!');
+} else {
+  console.log(firstName + ' will hopefully marry soon.')
+}
